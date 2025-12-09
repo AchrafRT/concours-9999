d5bd57dc655366e2cddb0742baf651a78d38847d
SayF <103608061+AchrafRT@users.noreply.github.com>
2025-12-09 12:42:24 -0500

    Update app.py
    geotest 0.9

--- app.py
+++ app.py
@@ -156,8 +156,8 @@ def signup():
     # -----------------------------------------
     # GEO-LOCK: MUST BE AT SPECIFIC LOCATION
     # -----------------------------------------
-    TARGET_LAT = 46.77289
-    TARGET_LON = -71.298241
+    TARGET_LAT = 46.805894
+    TARGET_LON = -71.3330326
     MAX_DISTANCE_KM = 1.0  # 250 m radius
 
     user_lat = request.form.get("lat")
